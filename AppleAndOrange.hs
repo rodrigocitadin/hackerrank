8347934cbca075fec673ffddee378db4821701d3
rodrigocitadin <citadinrodrigo@gmail.com>
2023-02-27 15:05:52 -0300

    uwu!

---
+++ AppleAndOrange.hs
@@ -0,0 +1,6 @@
+solve :: [Int] -> [Int]
+solve (s:t:a:b:m:_:rest) = [as, os]
+    where as = length $ filter (\x -> s <= x && x <= t) $ map (\x -> x + a) $ take m rest
+          os = length $ filter (\x -> s <= x && x <= t) $ map (\x -> x + b) $ drop m rest
+
+main = interact $ unlines . map show . solve . map read . words
